b415ccdcdfc3a82aba42793e6b38f27664340241
Jacob Schwartz <jtschwartz98@gmail.com>
2018-11-27 20:44:25 -0500

    Complete

--- rawsocket.py
+++ rawsocket.py
@@ -1,19 +1,25 @@
 import select
 import socket
 import struct
+import binascii
+import time
+
+_TIMEOUT = 15
+_ICMP_ECHO = 8
 
 
 class RawSocket:
 
 	def __init__(self, host):
 		self.sock = None
-		self.entry = host
-		self.host = self.get_ip()
-		self.ttl = 3
+		self.host = host
+		self.ip = self.get_ip()
+		self.ttl = 1
 		self.port = 33435
 		self.id = 6735
 		self.seq_number = 0
 		self.trace = list()
+		self.sent = None
 
 	def create_socket(self):
 		try:
@@ -24,7 +30,7 @@ class RawSocket:
 			exit(1)
 
 	def get_ip(self):
-		return socket.gethostbyname(self.entry)
+		return socket.gethostbyname(self.host)
 
 	@staticmethod
 	def checksum(packet):
@@ -42,47 +48,64 @@ class RawSocket:
 		checksum = ~checksum & 0xffff  # Invert and truncate to 16 bits
 		return socket.htons(checksum)
 
-	def sendPing(self, ttl):
+	def send_ping(self, ttl):
 		""" Create an icmp packet first, then use raw socket to sendto this packet """
 		# Header has 8 bytes: type (8), code (8), checksum (16), id (16), sequence (16)
 		checksum = 0  # initialize checksum to 0
 		# Make a dummy header with a 0 checksum.
-		header = struct.pack("!BBHHH", 8, 0, checksum, self.id, self.seq_number)
-		data = bytes(556)  # bytes of zeros
+		header = struct.pack("!BBHHH", _ICMP_ECHO, 0, checksum, self.id, self.seq_number)
+		data = bytes(64)  # bytes of zeros
 		packet = header + data
 		checksum = self.checksum(packet)  # compute checksum, in network order
 		# Now that we have the right checksum, put that in. Create a new header
-		header = struct.pack(…, checksum, …)  # fill in …
+		header = struct.pack("!BBHHH", _ICMP_ECHO, 0, checksum, self.id, self.seq_number)
 		packet = header + data  # Packet ready
 		self.ttl = ttl
 		self.sock.setsockopt(socket.SOL_IP, socket.IP_TTL, self.ttl)
 
 		try:
+			self.sent = time.time()
 			num = self.sock.sendto(packet, (self.ip, self.port))
-			print("bytes sent: ", num)  # print msg for debugging!!!!
-		except socket.error:
-			return None
+			# print("bytes sent: ", num)  # print msg for debugging!!!!
+			# print(binascii.hexlify(packet))
+		except socket.error as e:
+			print("Socket Error", e)
+			print(binascii.hexlify(packet))
 
-	def recv_ping(self):
+	def recv_ping(self, attempts=3):
 		"""Set timeout on receiving reply, call recvfrom(),
 		interpret header info return True if reach destination """
 		self.sock.setblocking(0)
-		data_ready = select.select([self.sock], [], [], TIMEOUT)
+		data_ready = select.select([self.sock], [], [], _TIMEOUT)
 		if not data_ready[0]:  # Timeout
-			print("recvfrom Timeout!")
-			return False
+			if attempts > 0:
+				return self.recv_ping(attempts - 1)
+			else:
+				print("recvfrom Timeout!")
+				return 0, 0, 0, 0
 
 		recPacket = b''  # empty bytes
-		recPacket, addr = self.sock.recvfrom(ICMP_MAX_RECV)
-		print("bytes received: ", len(recPacket))  # print for debugging!!!
+		recPacket, addr = self.sock.recvfrom(576)
+		# print("bytes received: ", len(recPacket))  # print for debugging!!!
 		# first 20 bytes in recv pkt are IP header that contains router/destination IP
 		ipHeader = recPacket[:20]
+		iphSrcIP = [0] * 4
 		iphVersion, iphTypeOfSvc, iphLength, \
 		iphID, iphFlags, iphTTL, iphProtocol, \
-		iphChecksum, iphSrcIP, iphDestIP = struct.unpack("!BBHHHBBHII", ipHeader)
+		iphChecksum, iphSrcIP[0], iphSrcIP[1], iphSrcIP[2], iphSrcIP[3], iphDestIP = struct.unpack("!BBHHHBBHBBBBI", ipHeader)
 		# next 8 bytes are ICMP reply header
 		icmpHeader = recPacket[20:28]
 		icmpType, icmpCode, icmpChecksum, \
 		icmpPacketID, icmpSeqNumber = struct.unpack("!BBHHH", icmpHeader)
-		print("icmpType = ", icmpType)  # for bebugging!!
-		print("icmpCode = ", icmpCode)
+		# print("icmpType = ", icmpType)  # for bebugging!!
+		# print("icmpCode = ", icmpCode)
+		src_ip = '.'.join(map(str, iphSrcIP))
+		# print(src_ip)
+		return self.get_host(src_ip), src_ip, (time.time() - self.sent), 4 - attempts
+
+	@staticmethod
+	def get_host(ip):
+		try:
+			return socket.gethostbyaddr(ip)[0]
+		except socket.error:
+			return "<no dns entry>"
--- tracert.py
+++ tracert.py
@@ -1,16 +1,28 @@
 # tracert.py
+import time
 
 from rawsocket import RawSocket
 import sys
 
 
 def main(host):
+	start_time = time.time()
 	connection = RawSocket(host)
-	connection.create_socket()
-	print("Tracing route to {}".format(connection.host))
+	print("Tracing route to {}".format(connection.ip))
 
-	for line in range(0, len(connection.trace), 1):
-		print("{}\t{}".format(line + 1, connection.trace[line]))
+	for hop in range(1, 31):
+		connection.create_socket()
+		connection.send_ping(hop)
+		dns, ip, rtt, probes = connection.recv_ping()
+		if probes == 0:
+			break  # TODO: Retransmit packets
+
+		print(f"{hop}\t{dns}\t({ip})\t{rtt:.3f}ms\t({probes})")
+
+		if ip == connection.ip:
+			break
+
+	print("Total execution time: {0:.3f}ms".format(time.time() - start_time))
 
 
 if __name__ == '__main__':
